8fd63d928dda03b48270c8839f6d5a5c198312d6
Bohdan Kovalchuk <bohd@cypress.com>
2019-10-11 16:09:45 +0300

    - flash-layer organization
    # Conflicts:
    #	boot/cypress/cy_flash_pal/cy_flash_map.c
    #	boot/cypress/cy_flash_pal/cy_flash_pal.h
    #	boot/cypress/cy_flash_pal/flash_map_backend/flash_map_backend.h

--- boot/cypress/cy_flash_pal/flash_map_backend/flash_map_backend.h
+++ boot/cypress/cy_flash_pal/flash_map_backend/flash_map_backend.h
@@ -4,9 +4,32 @@
  *
  * SPDX-License-Identifier: Apache-2.0
  */
-#ifndef FLASH_MAP_BACKEND_H
-#define FLASH_MAP_BACKEND_H
+#ifndef __FLASH_MAP_BACKEND_H__
+#define __FLASH_MAP_BACKEND_H__
 
-#include <flash_map/flash_map.h>
+#include <mcuboot_config/mcuboot_config.h>
 
-#endif /* FLASH_MAP_BACKEND_H */
+/*< Opens the area for use. id is one of the `fa_id`s */
+int     flash_area_open(uint8_t id, const struct flash_area **);
+void    flash_area_close(const struct flash_area *);
+/*< Reads `len` bytes of flash memory at `off` to the buffer at `dst` */
+int     flash_area_read(const struct flash_area *, uint32_t off, void *dst,
+                     uint32_t len);
+/*< Writes `len` bytes of flash memory at `off` from the buffer at `src` */
+int     flash_area_write(const struct flash_area *, uint32_t off,
+                     const void *src, uint32_t len);
+/*< Erases `len` bytes of flash memory at `off` */
+int     flash_area_erase(const struct flash_area *, uint32_t off, uint32_t len);
+/*< Returns this `flash_area`s alignment */
+uint8_t flash_area_align(const struct flash_area *);
+/*< Initializes an array of flash_area elements for the slot's sectors */
+int     flash_area_to_sectors(int idx, int *cnt, struct flash_area *ret);
+/*< Returns the `fa_id` for slot, where slot is 0 (primary) or 1 (secondary) */
+int     flash_area_id_from_image_slot(int slot);
+/*< Returns the slot, for the `fa_id` supplied */
+int     flash_area_id_to_image_slot(int area_id);
+
+//int flash_area_id_from_multi_image_slot(int image_index, int slot);
+//int flash_area_id_to_multi_image_slot(int image_index, int area_id);
+
+#endif /* __FLASH_MAP_BACKEND_H__ */
