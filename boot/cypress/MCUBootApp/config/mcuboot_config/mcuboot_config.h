660895fd9773c225a8518e538100f64b9c1dce11
Roman Okhrimenko <roman.okhrimenko@cypress.com>
2019-10-11 13:13:52 +0300

    Reverted back default mcuboot_config.h file to start from

--- boot/cypress/MCUBootApp/config/mcuboot_config/mcuboot_config.h
+++ boot/cypress/MCUBootApp/config/mcuboot_config/mcuboot_config.h
@@ -1,14 +1,13 @@
-/* Copyright 2019 Cypress Semiconductor Corporation
- *
+/*
  * Copyright (c) 2018 Open Source Foundries Limited
+ * Copyright (c) 2019 Arm Limited
  *
  * SPDX-License-Identifier: Apache-2.0
  */
-#ifndef MCUBOOT_CONFIG_H
-#define MCUBOOT_CONFIG_H
 
-#include "cy_flash.h"
-#include "flash_map/flash_map.h"
+#ifndef __MCUBOOT_CONFIG_H__
+#define __MCUBOOT_CONFIG_H__
+
 /*
  * Template configuration file for MCUboot.
  *
@@ -21,9 +20,6 @@
  * boot/zephyr/include/mcuboot_config/mcuboot_config.h
  * boot/mynewt/mcuboot_config/include/mcuboot_config/mcuboot_config.h
  */
-/* Default maximum number of flash sectors per image slot; change
- * as desirable. */
-#define MCUBOOT_MAX_IMG_SECTORS 128
 
 /*
  * Signature types
@@ -32,7 +28,7 @@
  */
 
 /* Uncomment for RSA signature support */
-//#define MCUBOOT_SIGN_RSA
+/* #define MCUBOOT_SIGN_RSA */
 
 /* Uncomment for ECDSA signatures using curve P-256. */
 #define MCUBOOT_SIGN_EC256
@@ -50,7 +46,7 @@
 #define MCUBOOT_OVERWRITE_ONLY
 
 #ifdef MCUBOOT_OVERWRITE_ONLY
-/* Uncomment to only erase and overwrite those slot 0 sectors needed
+/* Uncomment to only erase and overwrite those primary slot sectors needed
  * to install the new image, rather than the entire image slot. */
 /* #define MCUBOOT_OVERWRITE_ONLY_FAST */
 #endif
@@ -63,8 +59,8 @@
  * available.
  */
 
-/* Uncomment to use ARM's mbedCrypto cryptographic primitives */
-#define MCUBOOT_USE_MBED_TLS
+/* Uncomment to use ARM's mbedTLS cryptographic primitives */
+/* #define MCUBOOT_USE_MBED_TLS */
 /* Uncomment to use Tinycrypt's. */
 /* #define MCUBOOT_USE_TINYCRYPT */
 
@@ -73,7 +69,7 @@
  * even if no upgrade was performed. This is recommended if the boot
  * time penalty is acceptable.
  */
-#define MCUBOOT_VALIDATE_SLOT0
+#define MCUBOOT_VALIDATE_PRIMARY_SLOT
 
 /*
  * Flash abstraction
@@ -81,15 +77,15 @@
 
 /* Uncomment if your flash map API supports flash_area_get_sectors().
  * See the flash APIs for more details. */
-// TODO: FWSECURITY-755
 #define MCUBOOT_USE_FLASH_AREA_GET_SECTORS
 
 /* Default maximum number of flash sectors per image slot; change
  * as desirable. */
-extern struct flash_map_entry part_map[];
+define MCUBOOT_MAX_IMG_SECTORS 128
 
-/* Image Size / 512b sector WR/RD by PSoC6 FlashDriver */
-inline size_t Cy_BootMaxImgSectors(void) {return ((part_map[0].area.fa_size)/CY_FLASH_SIZEOF_ROW);}
+/* Default number of separately updateable images; change in case of
+ * multiple images. */
+#define MCUBOOT_IMAGE_NUMBER 1
 
 /*
  * Logging
@@ -99,38 +95,22 @@ inline size_t Cy_BootMaxImgSectors(void) {return ((part_map[0].area.fa_size)/CY_
  * If logging is enabled the following functions must be defined by the
  * platform:
  *
+ *    MCUBOOT_LOG_MODULE_REGISTER(domain)
+ *      Register a new log module and add the current C file to it.
+ *
+ *    MCUBOOT_LOG_MODULE_DECLARE(domain)
+ *      Add the current C file to an existing log module.
+ *
  *    MCUBOOT_LOG_ERR(...)
  *    MCUBOOT_LOG_WRN(...)
  *    MCUBOOT_LOG_INF(...)
  *    MCUBOOT_LOG_DBG(...)
  *
- * The following global logging level configuration macros must also be
- * defined, each with a unique value. Those will be used to define a global
- * configuration and will allow any source files to override the global
- * configuration:
- *
- *    MCUBOOT_LOG_LEVEL_OFF
- *    MCUBOOT_LOG_LEVEL_ERROR
- *    MCUBOOT_LOG_LEVEL_WARNING
- *    MCUBOOT_LOG_LEVEL_INFO
- *    MCUBOOT_LOG_LEVEL_DEBUG
- *
- * The global logging level must be defined, with one of the previously defined
- * logging levels:
- *
- *    #define MCUBOOT_LOG_LEVEL MCUBOOT_LOG_LEVEL_(OFF|ERROR|WARNING|INFO|DEBUG)
- *
- * MCUBOOT_LOG_LEVEL sets the minimum level that will be logged. The function
- * priority is:
+ * The function priority is:
  *
  *    MCUBOOT_LOG_ERR > MCUBOOT_LOG_WRN > MCUBOOT_LOG_INF > MCUBOOT_LOG_DBG
- *
- * NOTE: Each source file is still able to request its own logging level by
- * defining BOOT_LOG_LEVEL before #including `bootutil_log.h`
  */
 #define MCUBOOT_HAVE_LOGGING 1
-
-#define MCUBOOT_ROLLBACK_PROTECTION
 
 /*
  * Assertions
@@ -139,14 +119,19 @@ inline size_t Cy_BootMaxImgSectors(void) {return ((part_map[0].area.fa_size)/CY_
 /* Uncomment if your platform has its own mcuboot_config/mcuboot_assert.h.
  * If so, it must provide an ASSERT macro for use by bootutil. Otherwise,
  * "assert" is used. */
-#define MCUBOOT_HAVE_ASSERT_H
+/* #define MCUBOOT_HAVE_ASSERT_H */
 
-//#ifdef MCUBOOT_SIGN_RSA
-//#error "RSA is not supported in this release."
-//#endif
+/*
+ * Watchdog feeding
+ */
 
-#ifdef MCUBOOT_SIGN_EC
-#error "EC256 supported only."
-#endif
+/* This macro might be implemented if the OS / HW watchdog is enabled while
+ * doing a swap upgrade and the time it takes for a swapping is long enough
+ * to cause an unwanted reset. If implementing this, the OS main.c must also
+ * enable the watchdog (if required)!
+ *
+ * #define MCUBOOT_WATCHDOG_FEED()
+ *    do { do watchdog feeding here! } while (0)
+ */
 
-#endif /* MCUBOOT_CONFIG_H */
+#endif /* __MCUBOOT_CONFIG_H__ */
